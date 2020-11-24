1bc222f53c47acb34e04dddd7cacdd64cd907725
baldan.federico <baldan.federico@TAVE-PC11.OSDBSESTO.local>
2020-11-24 13:39:10 +0100

    add self invocking

---
+++ src/javathread/Processo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author baldan.federico
+ */
+public class Processo extends Thread {
+ private String a;
+ 
+    public Processo(String a) {
+        this.a=a;
+        this.start();
+    }
+    
+   public void run(){
+       for (;;) 
+           System.out.println(a);
+   }
+}
+   
